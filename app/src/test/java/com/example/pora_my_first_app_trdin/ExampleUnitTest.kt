56be76248673e241253f551eb78f72571638e412
Peter <trdin.peter@gmail.com>
2022-10-16 18:16:30 +0200

    first commit

---
+++ app/src/test/java/com/example/pora_my_first_app_trdin/ExampleUnitTest.kt
@@ -0,0 +1,17 @@
+package com.example.pora_my_first_app_trdin
+
+import org.junit.Test
+
+import org.junit.Assert.*
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+class ExampleUnitTest {
+    @Test
+    fun addition_isCorrect() {
+        assertEquals(4, 2 + 2)
+    }
+}
